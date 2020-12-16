9a630d1d99b013e8fe16e50d3e7dd179b0fa29bc
Ileana Tressa <itressa@un-b6.kzn.21-school.ru>
2020-12-16 18:24:05 +0300

    loadbalancer is on single ip now; also added /wordpress and /phpmyadmin for nginx but they dont work correctly

--- srcs/wordpress/srcs/onload.sh
+++ srcs/wordpress/srcs/onload.sh
@@ -7,7 +7,7 @@ do
     wp config create --dbhost=mysql --dbname=wordpress --dbuser=wp_user --dbpass=password_wp
 done;
 if ! wp core is-installed ; then
-    wp core install --url=http://192.168.99.193:5050 --title="itressa's k8s services" --admin_user="itressa" --admin_password="itressa" --admin_email="itressa@email.com" --skip-email
+    wp core install --url=http://192.168.99.21:5050 --title="itressa's k8s services" --admin_user="itressa" --admin_password="itressa" --admin_email="itressa@email.com" --skip-email
     for NAME in editor author contributor ; do
         wp user create $NAME $NAME@itressa.test --role=$NAME --user_pass=$NAME
     done
