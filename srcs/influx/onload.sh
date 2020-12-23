f5e7e46d910dab376d7a4a4c7507afecf82b9cc1
Ileana Tressa <itressa@si-n4.kzn.21-school.ru>
2020-12-23 21:28:34 +0300

    fixed influx+telegraf

--- srcs/influx/onload.sh
+++ srcs/influx/onload.sh
@@ -1,12 +1,12 @@
 influxd &
-set_error() { return 1;}
-set_error;
-until [ $? -eq 0 ]
-do
-    sleep 3;
-    influx setup -uadmin -badminb -oadmino -padmin007 -r0 -f;
-done
-killall influxd
-export INFLUX_TOKEN=$(influx auth list | grep "admin's Token" | cut -d$'\t' -f3)
-/telegraf-1.17.0/usr/bin/telegraf --config=/telegraf-1.17.0/telegraf.conf
-while true; do sleep 3; done
+(telegraf --input-filter docker --output-filter influxdb --aggregator-filter none \
+ --processor-filter none config | grep -vE "^ *#|^$") > /etc/telegraf/telegraf.conf
+# INFLUX_TOKEN=$(influx auth list | grep "admin's Token" | cut -d$'\t' -f3)
+# echo "
+# [[outputs.influxdb_v2]]
+#  urls = [\"http://localhost:8086\"]
+#  token = \"$INFLUX_TOKEN\"
+#  organization = \"admino\"
+#  bucket = \"adminb\"
+# " >> /etc/telegraf/telegraf.conf
+telegraf
